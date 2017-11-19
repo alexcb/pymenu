a0b17b989ff087792e375ff0bf30db4fa3d83c32
Alex Couture-Beil <alex@mofo.ca>
2017-11-19 11:19:34 -0800

    menu code

---
+++ example.py
@@ -0,0 +1,18 @@
+from pymenu.menu import menu
+
+# Press left/right to navigate between foo/bar menus
+# up/down between items
+x = [
+    ('foo', [1,2,3,4]),
+    ('bar', ['aa', 'bb', 'cc']),
+    ]
+
+foo = None
+def on_enter(selected):
+    global foo
+    foo =  selected
+    return True
+menu(x, on_enter)
+
+print foo
+
