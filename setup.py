9dff756fb1d5b553be8df5592bba877a7ed2491e
Alex Couture-Beil <alex@mofo.ca>
2018-05-11 12:24:48 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+import re
+from setuptools import find_packages, setup
+
+setup(
+    name='pymenu',
+    version='0.0.1',
+    description='ncurses menu',
+    long_description='ncurses menu',
+    url='https://github.com/alexcb/pymenu',
+    author='Alex Couture-Beil and contributors',
+    license='BSD',
+    packages=find_packages(),
+)
